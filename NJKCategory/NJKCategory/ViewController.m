535e551b68152c0659e57c4ac54fb46a28ce3bc6
jiangkuoniu <707429313@qq.com>
2020-08-21 14:27:38 +0800

    更改

--- NJKCategory/NJKCategory/ViewController.m
+++ NJKCategory/NJKCategory/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-#import "UIButton+JKNButton.h"
+
+#import "NJK_Kit.h"
 
 @interface ViewController ()
 
@@ -17,18 +18,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
     
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.njk_backgroundColor([UIColor whiteColor]);
     
-    UIButton * button  = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
-    
-    
-    button.backgroundColor = [UIColor greenColor];
-    
-    NSLog(@"%@",button.textTitle);
-    
+    UIButton * button = [UIButton new];
+    button.frame = CGRectMake(100, 100, 100, 100);
+    [button setEnlargeEdgeWithTop:20 right:20 bottom:20 left:20];
     [self.view addSubview:button];
+    
+    UIView * redView = [[UIView alloc] initWithFrame:CGRectMake(150, 200, 100, 100)];
+    redView.njk_backgroundColor([UIColor redColor]);
+    [redView viewShadowPathWithColor:[UIColor blueColor] shadowPathType:LeShadowPathAround shadowOpacity:10 shadowRadius:100 shadowPathWidth:100];
+    [self.view addSubview:redView];
 }
 
 
--- NJK_Category.podspec
+++ NJK_Category.podspec
@@ -1,11 +1,11 @@
 Pod::Spec.new do |s|
 s.name         = 'NJK_Category'
-s.version      = '0.0.1'
+s.version      = '0.0.2'
 s.summary      = '常用类扩展'
 s.homepage     = 'https://github.com/jiangkuoniu/NJKDemo'
 s.license      = 'MIT'
-s.authors      = {'MrXiaoYang' => '582385082@qq.com'}
-s.platform     = :ios, '6.0'
+s.authors      = {'jiangkuoniu' => '707429313@qq.com'}
+s.platform     = :ios, '9.0'
 s.source       = {:git => 'https://github.com/jiangkuoniu/NJKDemo.git', :tag => s.version}
 s.source_files = 'NJKCategor/NJK_Category/**/*'
 s.requires_arc = true
